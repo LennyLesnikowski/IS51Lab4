53dfe7b9072bf2fa74a01ef2ca89a2ca05aa4b2f
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-04-21 15:03:31 -0700

    structured english

--- Lab4.py
+++ Lab4.py
@@ -10,42 +10,6 @@ If the user fails to guess the correct answer within three tries, then print
 "You have used up all your guesses"
 
 """
-"""Psudocode
-main
-  question= "what is the capital of California"
-  ask(question, answer)
-
-ask
-  tries=0
-  loop three times
-        tries+=1
-        ask user input()
-        check to see if input=answer
-            if so, print "correct"
-            end
-    if not correct
-        print to the user "You have used up all your guesses"
-        print the correct answer "The correct answer is Sacramento"
-
-main
-"""
-def main():
-    question= "What is the captial of California? "
-    answer = "Sacramento"
-    ask(question, answer)
-def ask (question, answer, maxTries=3):
-    tries = 0
-    ans =""
-    while tries < maxTries:
-        tries =tries+1
-        ans = input(question)
-        if ans==answer:
-            print("correct")
-            break
-    if ans != answer:
-        print("You have used up your allotment of guesses.")
-    
-main()
 
 
 

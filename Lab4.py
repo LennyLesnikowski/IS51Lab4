b19f43f237777371c81f449d2ae7e02026a09663
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-04-21 15:05:55 -0700

    full psudocode

--- Lab4.py
+++ Lab4.py
@@ -1,14 +1,24 @@
-""" Lamens
-This program gives the user three tries to answer the quesiton 
-question = "what is the capital of California". With the answer being Sacramento
-We have to set a value for the three tries they get: max_tries = 3.
-Then we create a loop to iterate three times. For each iteration, we ask the user for the answer. 
-Then based on the answer the user gives, we check to see if the user input matches the correct answer. 
-If so we print "correct!" then terminate the loop.
 
-If the user fails to guess the correct answer within three tries, then print
-"You have used up all your guesses"
 
+
+"""Psudocode
+main
+  question= "what is the capital of California"
+  ask(question, answer)
+
+ask
+  tries=0
+  loop three times
+        tries+=1
+        ask user input()
+        check to see if input=answer
+            if so, print "correct"
+            end
+    if not correct
+        print to the user "You have used up all your guesses"
+        print the correct answer "The correct answer is Sacramento"
+
+main
 """
 
 
